8e8a56d4e9308abddc2e52ff3b539db31a8155e6
sudheesh <sudheesh@qburst.com>
2017-06-21 16:26:56 +0530

    Added Contact root

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
 
-  get 'foo/baz'
+  root 'static_pages#home'
 
   get 'static_pages/home'
+
+  get 'static_pages/contact'
 
   get 'static_pages/help'
 
   get 'static_pages/about'
-
-  root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
